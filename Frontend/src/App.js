436de491af63007277f817d69143f405d77adf41
MemoLee <softwarewewo@gmail.com>
2022-10-15 11:42:08 +0300

    Selam

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from './App.css'
+
+const App = () => {
+  return (
+    <div className='container mx-auto'>
+      <h1 className='top'> Bu bizim ilk react projectimiz. </h1>
+      <div className='box-border h-32 w-32 p-2 border-8'>
+        erdem yüksel blog site
+      </div>
+    </div>
+  )
+}
+
+export default App
